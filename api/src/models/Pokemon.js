b0d1c2fb11b9dd8346f3583a087a232c934de305
erikayeah <erikafogar.j@gmail.com>
2024-01-02 16:51:23 +0100

    first commit

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      speed: {
+        type: DataTypes.STRING,
+      },
+      height: {
+        type: DataTypes.STRING,
+      },
+      weighth: {
+        type: DataTypes.STRING,
+      },
+    },
+    { timestamps: false }
+  );
+};
---
+++ api/src/models/Type.js
@@ -0,0 +1,19 @@
+const { DataTypes, UUIDV4 } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "type",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: { type: DataTypes.STRING, 
+         allowNull: false 
+      }
+    },
+    { timestamps: false }
+  );
+};
