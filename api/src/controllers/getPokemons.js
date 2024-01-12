b4a730a22d1b19ca6bb4be664fd6c98675a90f5e
erikayeah <erikafogar.j@gmail.com>
2024-01-13 00:44:39 +0100

    filters independent and orders

--- api/src/controllers/getPokemons.js
+++ api/src/controllers/getPokemons.js
@@ -9,7 +9,7 @@ const getDataDb = require("../utils/getDataDb");
 
 
 const URL = "https://pokeapi.co/api/v2/pokemon/";
-const limit = 150;
+const limit = 250;
 let endpoint = URL
 
 const getPokemons = async () => {
