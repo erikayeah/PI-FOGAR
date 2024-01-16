4837f93c85d7ce568ef3d2d8176e07f3ae5cb9de
erikayeah <erikafogar.j@gmail.com>
2024-01-16 02:25:45 +0100

    CRUD

--- api/src/controllers/putPokemon.js
+++ api/src/controllers/putPokemon.js
@@ -1,5 +1,5 @@
 // pokemonController.js
-const { Pokemons}  = require('../db');
+const { Pokemons, Type } = require('../db');
 
 const putPokemon = async (id, updatedData) => {
   try {
@@ -15,8 +15,29 @@ const putPokemon = async (id, updatedData) => {
       throw new Error('No data provided for update');
     }
 
+    // Convertir cadenas vacías a null
+    Object.keys(updatedData).forEach((key) => {
+      if (updatedData[key] === '') {
+        updatedData[key] = null;
+      }
+    });
+
     // Actualizar los campos según los datos proporcionados
     Object.assign(pokemonToUpdate, updatedData);
+
+    // Manejar la relación de muchos a muchos
+    if (updatedData.types && Array.isArray(updatedData.types)) {
+      // Obtener los UUIDs de los tipos correspondientes
+      const typeUUIDs = await Type.findAll({
+        where: { name: updatedData.types }
+      });
+
+      // Limpiar tipos existentes para el Pokémon
+      await pokemonToUpdate.setTypes([], { through: 'pokemon_type' });
+
+      // Agregar los nuevos tipos
+      await pokemonToUpdate.addTypes(typeUUIDs, { through: 'pokemon_type' });
+    }
 
     // Guardar cambios en la base de datos
     await pokemonToUpdate.save();
--- api/src/handlers/putPokemonHandler.js
+++ api/src/handlers/putPokemonHandler.js
@@ -6,8 +6,8 @@ const putPokemonHandler = async (req, res, next) => {
     const { id } = req.params;
     const updatedData = req.body;
 
-    console.log('id en habnlder', id);
-    console.log('body en hanlder', updatedData);
+    // console.log('id en habndler', id);
+    // console.log('body en handler', updatedData);
 
     const updatedPokemon = await putPokemon(id, updatedData); // Puedes pasar los datos actualizados en el cuerpo de la solicitud
     res.status(200).json(updatedPokemon);
