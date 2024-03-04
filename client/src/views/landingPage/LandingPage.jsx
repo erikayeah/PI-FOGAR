a1be13562e4f1b9c0a008540735b6cc0cbaa9eb5
ErikaFogar <erikafogar.j@gmail.com>
2024-03-04 22:19:24 +0100

    landing

--- client/src/views/landingPage/LandingPage.jsx
+++ client/src/views/landingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
 //Style
 import styles from "./LandingPage.module.css";
+import landing from "../../assets/images/fondoFon.jpg";
+import logo from "../../assets/images/logo.png";
 
 //Hooks
 import { useNavigate } from "react-router-dom";
@@ -9,7 +11,7 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.body.style.backgroundImage = `url('/src/assets/images/fondoFon.jpg')`;
+    document.body.style.backgroundImage = { landing };
     return () => {
       document.body.style.backgroundImage = null;
     };
@@ -21,7 +23,7 @@ const LandingPage = () => {
 
   return (
     <div className={styles.container}>
-      <img className={styles.image} src="/src/assets/images/logo.png" alt="" />
+      <img className={styles.image} src={logo} alt="" />
       <button className={styles.btn} onClick={handleHome}>
         <span className={styles.noselect}>Gotta catch 'em all!</span>
       </button>
